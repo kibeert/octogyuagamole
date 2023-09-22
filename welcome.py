07c36186cac9356559c07f4e144f3663420758a9
kibeert <ckibertkoech@gmail.com>
2023-09-22 13:49:34 +0300

    ``` Add print statements for arr2.ndim, arr3[2], arr3[5], and x.
    ``` (generated by useblackbox.ai )

--- welcome.py
+++ welcome.py
@@ -5,3 +5,9 @@ print(arr)
 arr2= np.array([[[1,2,3],[4,5,6],[7, 8, 9]]])
 print(arr2)
 print(arr2.ndim)
+
+arr3= np.array([1,2,3,4,5,5,6])
+print(arr3[2])
+print(arr3[5])
+x = arr3[2] + arr3[5]
+print(x)
