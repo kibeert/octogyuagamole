93b3b8070c4705e1a1915996039bd92715ffad8e
kibeert <ckibertkoech@gmail.com>
2023-09-22 14:22:42 +0300

    ``` Split an array into multiple sub-arrays
    This commit splits an array into multiple sub-arrays using the `np.array_split()` function.
    The original array `a` is split into 3 sub-arrays, and the new array `newar` is split into 4 sub-arrays.
    The function `np.hsplit()` is used to split the array `arr` horizontally into 3 sub-arrays.
    The function `np.where()` is used to find the index of the elements in the array `ar` that are odd.
    ``` (generated by useblackbox.ai )

--- welcome.py
+++ welcome.py
@@ -53,3 +53,11 @@ print(b)
 ar = np.array([1,2,3,4,5,6])
 newar = np.array_split(ar, 4)
 print(newar)
+arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12], [13, 14, 15], [16, 17, 18]])
+
+newarr = np.hsplit(arr, 3)
+
+print(newarr)
+ar = np.array([1,2,3,4,5,6])
+x = np.where(ar%2 == 1)
+print("The index of the element is ", x )
