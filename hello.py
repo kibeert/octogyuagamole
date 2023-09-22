a769709415fed3b9df9e5636e0775f8c889f41c3
kibeert <ckibertkoech@gmail.com>
2023-09-22 15:57:55 +0300

    ``` JSON data is used instead of CSV data
    ``` (generated by useblackbox.ai )

--- hello.py
+++ hello.py
@@ -1,8 +1,3 @@
 import pandas as pd
-x= pd.read_csv("data.csv")
-print(x)
-print(pd.__version__)
-a = [1, 7, 2]
-myvar = pd.Series(a)
-print(myvar)
-print(myvar[0])
+df = pd.read_json("data.json")
+print(df)
