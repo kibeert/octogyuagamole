3b10263a5fa6f6913e6cc2457d0934ad7c585dd6
kibeert <ckibertkoech@gmail.com>
2023-09-22 14:18:02 +0300

    The following changes were made to welcome.py:
    * The print statement for arr3[5] was removed.
    * A print statement for arr2d[0,2] was added.
    * The dtype of arr2d was printed.
    * The dtype of arrs was printed.
    * A copy of arra was created and the first element was changed to 12. The original and copied arrays were then printed.
    * A view of arra2 was created and the first element was changed to 12. The original and viewed arrays were then printed.
    * The base of the copied and viewed arrays were printed.
    * The shape of arry2 was printed.
    * The elements of arry2 were printed using a for loop.
    * The elements of arry2 were printed using a nested for loop.
    * The elements of arry2 were printed using np.nditer.
    * The arrays array and array3 were concatenated and printed.
    * The array a was split into 3 arrays and printed.
    * The array ar was split into 4 arrays and printed. (generated by useblackbox.ai )

--- welcome.py
+++ welcome.py
@@ -13,3 +13,43 @@ x = arr3[2] + arr3[5]
 print(x)
 arr2d= np.array([[2,3,4,5],[6,7,8,9]])
 print(arr2d[0,2])
+print(arr2d.dtype)
+arrs = np.array(["apple", "banana", "cherry"], dtype="S")
+print(arrs.dtype)
+
+arra =  np.array([1,2,3,4,5])
+x =arra.copy()
+arra[0] = 12
+print(arra)
+print(x)
+arra2 = np.array([1,2,3,4,5])
+y =arra2.view()
+arra2[0]= 12
+print(arra2)
+print(y)
+x = arra2.copy()
+y = arra2.view()
+print(x.base)
+print(y.base)
+arry2= np.array([[[1,2,3],[4,5,6],[7, 8, 9]]])
+print(arry2.shape)
+for x in arry2:
+    print(x)
+
+for x in arry2:
+    for y in x :
+        for z in y:
+            print(z)
+
+for x in np.nditer(arry2):
+    print(x)
+
+array = np.array([1,2,3,4,5])
+array3 =np.array([7,8,9,0,12])
+a= np.concatenate((array, array3), axis=None)
+print(a)
+b = np.array_split(a, 3)
+print(b)
+ar = np.array([1,2,3,4,5,6])
+newar = np.array_split(ar, 4)
+print(newar)
