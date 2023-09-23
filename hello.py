3272b47df3664de6eda1f62af6bd6aba9698e366
kibeert <ckibertkoech@gmail.com>
2023-09-23 10:09:39 +0300

    Update data.csv and hello.py (generated by useblackbox.ai )

--- hello.py
+++ hello.py
@@ -1,5 +1,4 @@
 import pandas as pd
-df = pd.read_csv("data.csv")
-x=df["Date"].median()
-df["Date"].fillna(x, inplace=True)
-print(df)
+df = pd.read_csv('data.csv')
+df.loc[7, 'Duration']= 45
+print(df.to_string())
