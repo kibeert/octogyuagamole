6b55db49b23b12b84881ec655a71829017664630
kibeert <ckibertkoech@gmail.com>
2023-09-24 11:05:20 +0300

    ``` Added subplots to the hello.py script.
    ``` (generated by useblackbox.ai )

--- hello.py
+++ hello.py
@@ -1,16 +1,20 @@
+#displaying subplots
 import matplotlib.pyplot as plt
 import numpy as np
+x = np.array([0,1,2,3])
+y = np.array([3, 8, 1, 10])
 
-xpoints= np.array([1,2,6,8,15])
-ypoints= np.array([3, 8, 1, 10, 25])
-y2points= np.array([4,6,9,3,1])
+plt.subplot(3,1,1)
+plt.plot(x, y,'r') #red line
 
-font1 = {'family':'serif','color':'blue','size':20}
-font2 = {'family':'serif','color':'darkred','size':15}
-plt.plot(xpoints, ypoints, linestyle ="solid", color= "r")
-plt.plot(xpoints, y2points)
-plt.xlabel("x points", fontdict=font2)
-plt.ylabel("y points", fontdict=font2)
-plt.title("Graph plotting", fontdict=font1)
-plt.grid()
+x = np.array([0,1,2,3])
+y = np.array([10,20,30,40])
+plt.subplot(3,1,2)#second plot in a row and first column
+plt.plot(x,y,"b")
+
+x = np.array([0,1,2,3])
+y = np.array([10,20,30,40])
+plt.subplot(3,1,3)#second plot in a row and first column
+plt.plot(x,y,"b")
+
 plt.show()
