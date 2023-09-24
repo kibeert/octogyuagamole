4144da38b89fd741bcb0f6a5f235e8146d56f6f4
kibeert <ckibertkoech@gmail.com>
2023-09-24 10:42:24 +0300

    The diff adds a second line to the plot, plotting the points (xpoints, y2points). (generated by useblackbox.ai )

--- hello.py
+++ hello.py
@@ -3,5 +3,7 @@ import numpy as np
 
 xpoints= np.array([1,2,6,8,15])
 ypoints= np.array([3, 8, 1, 10, 25])
-plt.plot(xpoints, ypoints, linestyle ="solid", color= "r", lw= "20.5")
+y2points= np.array([4,6,9,3,1])
+plt.plot(xpoints, ypoints, linestyle ="solid", color= "r")
+plt.plot(xpoints, y2points)
 plt.show()
