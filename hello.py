bd95cf6104a9cc8ecc267820c6fda4cf106fff14
kibeert <ckibertkoech@gmail.com>
2023-09-24 11:16:13 +0300

    ``` Pie chart of 250 normally distributed data points with mean 170 and standard deviation 10.
    ``` (generated by useblackbox.ai )

--- hello.py
+++ hello.py
@@ -1,20 +1,7 @@
-#displaying subplots
 import matplotlib.pyplot as plt
 import numpy as np
-x = np.array([0,1,2,3])
-y = np.array([3, 8, 1, 10])
+import random
 
-plt.subplot(3,1,1)
-plt.plot(x, y,'r') #red line
-
-x = np.array([0,1,2,3])
-y = np.array([10,20,30,40])
-plt.subplot(3,1,2)#second plot in a row and first column
-plt.plot(x,y,"b")
-
-x = np.array([0,1,2,3])
-y = np.array([10,20,30,40])
-plt.subplot(3,1,3)#second plot in a row and first column
-plt.plot(x,y,"b")
-
+x= np.random.normal(170,10,250)
+plt.pie(x)
 plt.show()
