ebdd1ecabddb5eae795e6370638e73522bab8e70
kibeert <ckibertkoech@gmail.com>
2023-09-24 10:39:39 +0300

    Commit message:
    ```
    Changed the linestyle from dotted to solid and increased the linewidth to 20.5.
    ``` (generated by useblackbox.ai )

--- hello.py
+++ hello.py
@@ -3,5 +3,5 @@ import numpy as np
 
 xpoints= np.array([1,2,6,8,15])
 ypoints= np.array([3, 8, 1, 10, 25])
-plt.plot(xpoints, ypoints, linestyle ="dotted", color= "r")
+plt.plot(xpoints, ypoints, linestyle ="solid", color= "r", lw= "20.5")
 plt.show()
