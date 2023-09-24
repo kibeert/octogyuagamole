73cd0724a1512d5a54e1816219435f5b78310b02
kibeert <ckibertkoech@gmail.com>
2023-09-24 11:34:05 +0300

    The diff is about changing the data used to create a pie chart. The original data was generated randomly from a normal distribution with a mean of 170 and a standard deviation of 10. The new data is an array of five values: 0, 50, 100, 150, and 200. The labels for the new data are "Apples", "Bananas", "Cherries", "Dates", and "Oranges". The explode parameter is set to [0.2, 0, 0, 0, 0], which means that the "Apples" slice will be exploded by 20% and the other slices will not be exploded. (generated by useblackbox.ai )

--- hello.py
+++ hello.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import random
 
-x= np.random.normal(170,10,250)
-plt.pie(x)
+y = np.array([0, 50, 100, 150,200])
+mylabels = ["Apples", "Bananas", "Cherries", "Dates", "Oranges"]
+myexplode = [0.2, 0, 0, 0,0]
+plt.pie(y,labels= mylabels,  explode=myexplode)
 plt.show()
