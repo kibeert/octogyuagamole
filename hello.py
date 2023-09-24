d4b0e38931ffd2eb445560be1f4d1c63d0073864
kibeert <ckibertkoech@gmail.com>
2023-09-24 10:50:28 +0300

    The diff adds a title, labels to the x and y axes, and changes the font and color of the title and labels. (generated by useblackbox.ai )

--- hello.py
+++ hello.py
@@ -4,6 +4,12 @@ import numpy as np
 xpoints= np.array([1,2,6,8,15])
 ypoints= np.array([3, 8, 1, 10, 25])
 y2points= np.array([4,6,9,3,1])
+
+font1 = {'family':'serif','color':'blue','size':20}
+font2 = {'family':'serif','color':'darkred','size':15}
 plt.plot(xpoints, ypoints, linestyle ="solid", color= "r")
 plt.plot(xpoints, y2points)
+plt.xlabel("x points", fontdict=font2)
+plt.ylabel("y points", fontdict=font2)
+plt.title("Graph plotting", fontdict=font1)
 plt.show()
