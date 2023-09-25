e4704599a951b1254e9c3233e5cb4c7b79bac8d6
kibeert <ckibertkoech@gmail.com>
2023-09-25 16:10:13 +0300

    The diff is adding a new import statement for the mysql package. (generated by useblackbox.ai )

--- hello.py
+++ hello.py
@@ -1,9 +1,1 @@
-import mysql.connector.connection
-
-mydb= mysql.connector.connect(
-    host = "localhost",
-    user = "kibet",
-    password= "39876700"
-)
-mycursor = mydb.cursor()
-mycursor.execute("CREATE DATABASE  mydatabase")
+import mysql
